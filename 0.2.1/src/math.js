9b52b196783588219c55166fbfeccbcca70bb912
chrsmlls333 <chrsmlls333@gmail.com>
2021-10-06 03:39:29 -0500

    Add quantize functions to CEM math

--- 0.2.1/src/math.js
+++ 0.2.1/src/math.js
@@ -3,3 +3,11 @@
 export * from "./scalingfit.js";
 export * from "./random.js";
 export * from "./p5math.js";
+
+const findStep = (func, value, step) => {
+    if (step == 0) return 0; //throw?
+    return step * func(value / step);
+}
+export const roundStep = function(value, step = 1) { return findStep(Math.round, ...arguments) };
+export const floorStep = function(value, step = 1) { return findStep(Math.floor, ...arguments) };
+export const  ceilStep = function(value, step = 1) { return findStep( Math.ceil, ...arguments) };
