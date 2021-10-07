e5a7b47c256ebc01292265349e5ec556226a890b
chrsmlls333 <chrsmlls333@gmail.com>
2021-10-07 01:07:23 -0500

    Add vector math functions to CEM lib

--- 0.2.1/src/math.js
+++ 0.2.1/src/math.js
@@ -11,3 +11,13 @@ const findStep = (func, value, step) => {
 export const roundStep = function(value, step = 1) { return findStep(Math.round, ...arguments) };
 export const floorStep = function(value, step = 1) { return findStep(Math.floor, ...arguments) };
 export const  ceilStep = function(value, step = 1) { return findStep( Math.ceil, ...arguments) };
+
+const operateOnVecComponents = (vec, func) => {
+    if (!(vec instanceof p5.Vector)) throw "I actually only work with p5.Vector objects, sorry!"
+    const xyz = vec.array();
+    const newxyz = xyz.map(v => func(v));
+    return vec.set(newxyz);
+}
+export const roundVector = function(vec) { return operateOnVecComponents(vec, Math.round) };
+export const floorVector = function(vec) { return operateOnVecComponents(vec, Math.floor) };
+export const  ceilVector = function(vec) { return operateOnVecComponents(vec,  Math.ceil) };
