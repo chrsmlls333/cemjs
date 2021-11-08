80c679ff756b352cecf4bcc07f53666cc61e0825
chrsmlls333 <chrsmlls333@gmail.com>
2021-11-08 00:43:59 -0600

    Extract Grid creation to its own class/element, isolate page variable, change math of ray center slightly, change defaultAccumulator value, build out vector math module (which could be optimized)

--- 0.2.2/src/math/vectormath.js
+++ 0.2.2/src/math/vectormath.js
@@ -1,14 +1,33 @@
+import mapValues from "../../../submodules/lodash/mapValues.js";
 
-export const operateOnVecComponents = (vec, func) => {
-    if (!(vec instanceof p5.Vector)) 
-        throw new TypeError("I actually only work with p5.Vector objects, sorry!")
-    const xyz = vec.array();
-    const newxyz = xyz.map(v => func(v));
-    return vec.set(newxyz);
+export const mapVector = (vec, func) => {
+    if ((p5 && vec instanceof p5.Vector) ||
+        (vec && vec.x !== undefined && vec.y !== undefined)) {
+        return mapValues(vec, (v, k) => {
+            if (k === 'x' || k === 'y' || k === 'z' ) 
+                return func(v, k);
+            return v;
+        });
+    } else {
+        throw new TypeError("I actually only work with p5.Vector and Vector-like objects, sorry!")
+    }
 }
-export const roundVector = function(vec) { return operateOnVecComponents(vec, Math.round) };
-export const floorVector = function(vec) { return operateOnVecComponents(vec, Math.floor) };
-export const  ceilVector = function(vec) { return operateOnVecComponents(vec,  Math.ceil) };
+
+export const roundVector = (vec) => mapVector(vec, Math.round);
+export const floorVector = (vec) => mapVector(vec, Math.floor);
+export const  ceilVector = (vec) => mapVector(vec,  Math.ceil);
+
+export const Vec = (x = 0, y = 0) => ({x,y}); // Simple vector object
+export const vecMultScalar = (vec1, s) => mapVector(vec1, v => v * s );
+export const vecMultVec = (vec1, vec2) => mapVector(vec1, (v, k) => v * vec2[k]);
+export const vecDivScalar =  (vec1, s) => mapVector(vec1, v => v / s );
+export const vecDivVec =  (vec1, vec2) => mapVector(vec1, (v, k) => v / vec2[k]);
+export const vecAddScalar =  (vec1, s) => mapVector(vec1, v => v + s );
+export const vecAddVec =  (vec1, vec2) => mapVector(vec1, (v, k) => v + vec2[k]);
+export const vecSubScalar =  (vec1, s) => mapVector(vec1, v => v - s );
+export const vecSubVec =  (vec1, vec2) => mapVector(vec1, (v, k) => v - vec2[k]);
+
+
 
 export const dot = (v1, v2) => {
     let x1, y1, x2, y2
