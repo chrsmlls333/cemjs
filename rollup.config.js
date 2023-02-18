0149cbf73399a43bdfc105da587ee702e1220080
chrsmlls333 <chrsmlls333@gmail.com>
2023-02-18 03:52:14 -0600

    Preliminary bundler config

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+/**
+ * 
+ * Based on: https://gist.github.com/aleclarson/9900ed2a9a3119d865286b218e14d226
+ * 
+ */
+
+// import dts from 'rollup-plugin-dts'
+// import esbuild from 'rollup-plugin-esbuild'
+
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+import packageJson from './package.json' assert { type: 'json' }
+const name = packageJson.main.replace(/\.js$/, '')
+const input = packageJson.entry
+
+// const bundle = config => ({
+//   ...config,
+//   input,
+//   // external: id => !/^[./]/.test(id),
+// })
+
+export default [
+  {
+    input,
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      commonjs({ include: 'node_modules/**' })
+    ],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        name: 'cem',
+        file: `${name}.browser.js`,
+        format: 'iife',
+        sourcemap: true,
+      },
+    ],
+  },
+  // {
+  //   plugins: [dts()],
+  //   output: {
+  //     file: `${name}.d.ts`,
+  //     format: 'es',
+  //   },
+  // },
+]
