62c06f1986b2bd8b57eeb844f3b094c4db7d7e35
chrsmlls333 <chrsmlls333@gmail.com>
2023-02-21 01:59:22 -0600

    Add p5 to project types

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+import P5 = require('p5');
+// import * as p5Sound from 'p5/lib/addons/p5.sound'
+import * as p5Global from 'p5/global'
+
+export = P5;
+export as namespace p5;
+
+declare global {
+    interface Window {
+        p5: typeof P5,
+    }
+}
